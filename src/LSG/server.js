726a6ea0bec11b2cd7ea55e2ef2579727c0fb473
LSG <sukyeng97@naver.com>
2024-08-05 23:08:38 +0900

    server test

--- src/LSG/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mysql = require("mysql2/promise");
-
-app.listen(3000);
-app.use(bodyParser.json());
-
-app.get("/test", (req, res) => res.send("Hello World"));
